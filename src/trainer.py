254ec4e8829fa2fc71d5c3068c4efc57bdb0c46b
fadel <fadel.seydou@gmail.com>
2025-08-01 19:30:06 +0200

    update

--- src/trainer.py
+++ src/trainer.py
@@ -7,14 +7,15 @@ from sentence_transformers import SentenceTransformer
 from lightning import LightningModule, Trainer
 from lightning import LightningDataModule
 from lightning.pytorch.callbacks import ModelCheckpoint
-from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.loggers import TensorBoardLogger,MLFlowLogger
+import mlflow
 
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
+import torchvision.transforms.v2 as T
 import torch.nn as nn
 import torch.nn.functional as F
-
+from open_clip import create_model_from_pretrained, get_tokenizer 
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from scipy.stats import spearmanr
 
@@ -33,22 +34,16 @@ class FoodWasteTorchDataset(Dataset):
         s = self.view[self.ids[idx]]
         img = self.img_tfms(Image.open(s.filepath).convert("RGB"))
 
-        # ingredient embeddings (variable-length)
-        emb = torch.from_numpy(
-            np.stack([self.emb_matrix[self.ing2idx[i]] for i in s["ingredient_name"]])
-        )
-
         # regression target (g of waste per ingredient)
         tgt = torch.zeros(len(self.ing2idx), dtype=torch.float32)
         for ing, amt in zip(s["ingredient_name"], s["return_quantity"]):
             if amt is not None:
                 tgt[self.ing2idx[ing]] = amt
-        return img, emb, tgt
+        return img, s["ingredient_name"], tgt
 
 def collate(batch):
     imgs, embs, tgts = zip(*batch)
     return torch.stack(imgs), list(embs), torch.stack(tgts)
-
 
 class DataModule(LightningDataModule):
 
@@ -67,28 +62,25 @@ class DataModule(LightningDataModule):
         ing2idx = {ing: i for i, ing in enumerate(all_ing)}
         idx2ing = {i: ing for ing, i in ing2idx.items()}
         emb_matrix = self.sbert.encode(all_ing, convert_to_numpy=True)
-        NUM_ING = len(ing2idx)
+        self.num_ingredients = len(ing2idx)
+        self.ingredients = all_ing
 
-        self.get_weights(dataset)
-
-        val_tfms = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                            std=[0.229, 0.224, 0.225]),
+        val_tfms = T.Compose([
+        T.Resize((224, 224)),
+        T.ToImage(), 
+        T.ToDtype(torch.float32, scale=True),
+        #T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
         ])
 
-        train_tfms = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(45),
-        transforms.RandomAdjustSharpness(0.5),
-        transforms.RandomAdjustContrast(0.5),
-        transforms.RandomAdjustBrightness(0.2),
-        transforms.RandomAdjustSaturation(0.2),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                            std=[0.229, 0.224, 0.225]),
+        train_tfms = T.Compose([
+        T.Resize((224, 224)),
+        T.ToImage(), 
+        T.ToDtype(torch.float32, scale=True),
+        T.RandomHorizontalFlip(),
+        T.RandomRotation(45),
+        #T.AutoAugment(policy=T.AutoAugmentPolicy.IMAGENET),
+        T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1,),
+        #T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
         ])
 
         train_view = dataset.match({"split": "train"})
@@ -96,17 +88,7 @@ class DataModule(LightningDataModule):
         self.train_dataset = FoodWasteTorchDataset(train_view, train_tfms,ing2idx,emb_matrix)
         self.test_dataset  = FoodWasteTorchDataset(test_view, val_tfms,ing2idx,emb_matrix)
     
-    def get_weights(self,train_dataset,num_ing:int,device:str):
-        freq = torch.zeros(num_ing, dtype=torch.long)
-
-        for sample in train_dataset:
-            freq += (sample[2] > 0)
-
-        w = 1.0 / torch.log(freq.float() + 2)
-        w = w / w.mean()
-        w = w.to(device)
-        return w
-
+    
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True,collate_fn=collate)
 
@@ -117,61 +99,48 @@ class FoodWastePredictor(nn.Module):
     """
     Baseline: ResNet image encoder + pooled SBERT ingredient encoder
     """
-    def __init__(self, *, embedding_dim: int = 512, num_ingredients: int):
+    def __init__(self, *, labels_list: int):
         super().__init__()
 
         # ---------- image branch ----------
-        self.resnet = timm.create_model("timm/mobilenetv4_hybrid_large.ix_e600_r384_in1k",pretrained=True,num_classes=128)
+        self.model, self.preprocess = create_model_from_pretrained('hf-hub:timm/PE-Core-S-16-384')
+        self.tokenizer = get_tokenizer('hf-hub:timm/PE-Core-S-16-384')
+        self.text = self.tokenizer(labels_list, context_length=self.model.context_length)
 
-        # ---------- ingredient branch ----------
-        self.ing_mlp = nn.Sequential(                       # (B, 512) → (B, 128)
-            nn.Linear(embedding_dim, 256),
-            nn.ReLU(inplace=True),
-            nn.Linear(256, 128),
-            nn.ReLU(inplace=True)
-        )
+        self.num_ingredients = len(labels_list)
+        self.model.eval()  # set to eval mode
+        for p in self.model.parameters():
+            p.requires_grad = False
+        #self.text_features = self.model.encode_text(self.text, normalize=True)
 
-        # ---------- fusion & head ----------
-        self.fusion = nn.Sequential(
-            nn.Linear(128 + 128, 128),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.2)
-        )
-        self.q_proj = nn.Linear(128, embedding_dim, bias=False)
-        self.regressor   = nn.Linear(128, num_ingredients)  # (B,N_ing)
+        self.preprocess = T.Compose([
+            T.Resize((384, 384)),
+            T.ToDtype(torch.float32, scale=True),
+            T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ])
 
-    def forward(self, images, artikel_emb, return_total=True):
+        self.regressor = nn.LazyLinear(self.num_ingredients)  # (B,N_ing)
+
+    def forward(self, images:torch.Tensor, artikel_emb:list[list[str]], return_total=True):
         """
         images: Tensor [B, 3, H, W]
-        artikel_emb: Tensor [B, L, 512] or list of [L_i, 512] tensors
+        artikel_emb: 
         """
         # --- image path ---
-        img_feat = self.resnet(images)                      # (B,128)
-
-        # ----- text path -----
-        if isinstance(artikel_emb, list):                   # list → pad
-            artikel_emb = nn.utils.rnn.pad_sequence(
-                artikel_emb, batch_first=True)
-        # ing_feat = self.ing_mlp(artikel_emb.mean(dim=1))    # (B,128)
-        token_feat = self.ing_mlp(artikel_emb)              # (B,L,128)
-        query = self.q_proj(img_feat).unsqueeze(2)
-        attn_logits = (artikel_emb @ query).squeeze(2)
-        attn_weights = torch.softmax(attn_logits, dim=1)
-        ing_feat = (attn_weights.unsqueeze(2) * token_feat).sum(dim=1)  # (B,128)
-        # ----- fuse & slot predictions -----
-        combined      = torch.cat((img_feat, ing_feat), dim=1)
-        fused = self.fusion(combined)                       # (B, 128)
-        return F.relu(self.regressor(fused))
+        image = self.preprocess(images)
+        image_features = self.model.encode_image(image, normalize=True)
+        text_features = self.model.encode_text(artikel_emb, normalize=True)
+        
+        return self.regressor(image_features @ text_features.T)
 
 class FoodWasteModel(LightningModule):
 
-    def __init__(self,model:nn.Module,weight_vec:torch.Tensor,lr:float=1e-3):
+    def __init__(self,model:nn.Module,lr:float=1e-3):
         super().__init__()
 
         self.lr = lr
 
         self.model = model
-        self.weight_vec = weight_vec
         self.criterion  = nn.MSELoss(reduction='none')
 
         self.y_true, self.y_pred = [], []
@@ -189,23 +158,20 @@ class FoodWasteModel(LightningModule):
 
         images, ingredients, targets = batch
         images         = images.to(self.device)
-        self.weight_vec = self.weight_vec.to(self.device)
-        ingredients    = [seq.to(self.device) for seq in ingredients]
         targets  = targets.to(self.device)
         targets = self.jitter_targets(targets, epsilon=1.)
         outputs = self.forward(images, ingredients)
         tot_pred = outputs.sum(1)
         tot_true = targets.sum(1)
         diff_sq = self.criterion(outputs, targets)
-        loss = (diff_sq * self.weight_vec).mean()
+        loss = diff_sq .mean()
         total_loss = loss + 0.001 * F.l1_loss(tot_pred, tot_true)
-        self.log("train_loss",loss)
+        self.log("train_loss",total_loss,on_epoch=True)
         return loss
 
     def validation_step(self,batch,batch_idx):
         images, ingredients, targets = batch
         imgs     = images.to(self.device)
-        ingredients     = [ing.to(self.device) for ing in ingredients]
         targets  = targets.to(self.device)
         preds = self.forward(imgs, ingredients)
         self.y_true.append(targets.cpu().numpy())
@@ -225,9 +191,9 @@ class FoodWasteModel(LightningModule):
         rmse = np.sqrt(mse)
         mae  = mean_absolute_error(self.y_true, self.y_pred)
         r2   = r2_score(self.y_true, self.y_pred)
-        logs = {"MSE": mse, "RMSE": rmse, "MAE": mae, "R²": r2}
-        logs["MAE_total"] = mae_total
-        logs["rho"] = rho
+        logs = {"val_MSE": mse, "val_RMSE": rmse, "val_MAE": mae, "val_R2": r2}
+        logs["val_MAE_total"] = mae_total
+        logs["val_rho"] = rho
 
         self.log_dict(logs)
 
@@ -241,5 +207,34 @@ class FoodWasteModel(LightningModule):
 if __name__ == "__main__":
     data = DataModule()
     data.setup()
-    for _ in data.train_dataloader():
-        pass
+    #for _ in data.train_dataloader():
+    #    pass
+    model = FoodWasteModel(
+        model=FoodWastePredictor(labels_list=data.ingredients),
+        lr=1e-3
+    )
+
+    mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+
+    logger = MLFlowLogger(experiment_name="lightning_logs", tracking_uri="http://127.0.0.1:5000/")
+
+
+    trainer = Trainer(max_epochs=10, accelerator="auto", 
+                      precision=32,
+                      #fast_dev_run=True,
+                      callbacks=[
+                        ModelCheckpoint(
+                            dirpath="checkpoints",
+                            monitor="val_MAE",
+                            mode="min",
+                            save_top_k=1,
+                            save_on_train_epoch_end=False,
+                        )
+                    ],
+                    logger=logger,
+                      )
+
+    trainer.fit(
+        model,
+        data,        
+    )
